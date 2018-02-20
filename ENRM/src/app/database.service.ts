ee6cfe0088fc9641d703a4a546dbba34c84de017
God <phosawat.o@ku.th>
2018-02-20 20:38:18 +0700

    edit database.service

--- ENRM/src/app/database.service.ts
+++ ENRM/src/app/database.service.ts
@@ -12,6 +12,7 @@ export class DatabaseService {
   ) {
     this.config = mysqlConfig.config
     this.con = mysql.createConnection(this.config)
+    console.log(this.con)
   }
 
   dbConnection() {
