9fd929e21d7dc3ab9a79c1ae4a340a67f7f47b95
God <phosawat.o@ku.th>
2018-03-04 18:19:30 +0700

    test

--- mongodb/app.js
+++ mongodb/app.js
@@ -20,6 +20,9 @@ mongoose.connect('mongodb://127.0.0.1:27017/nerms', { useMongoClient: true })
   .then(() => { console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/nerms`) })
   .catch(() => { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/nerms`) })
 
+app.listen(3000, '0.0.0.0', function () {
+  console.log('Listening to port:  ' + 3000);
+});
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://158.108.34.72:4200");
@@ -61,10 +64,6 @@ app.use(function (err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
-
-app.listen(3000, '0.0.0.0', function () {
-  console.log('Listening to port:  ' + 3000);
 });
 
 module.exports = app;
