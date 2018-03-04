7f510d9fe2cfd4e011eff4a3e5955e8d8b47a3c9
God <phosawat.o@ku.th>
2018-03-04 18:16:16 +0700

    test

--- mongodb/app.js
+++ mongodb/app.js
@@ -16,12 +16,12 @@ var app = express();
 
 var mongoose = require('mongoose')
 mongoose.Promise = bluebird
-mongoose.connect('mongodb://127.0.0.1:27017/nerms', { useMongoClient: true })
-  .then(() => { console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/nerms`) })
-  .catch(() => { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/nerms`) })
+mongoose.connect('mongodb://127.0.0.1:27017/nerms', { useMongoClient: true})
+.then(()=> { console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/nerms`)})
+.catch(()=> { console.log(`Error Connecting to the Mongodb Database at URL : mongodb://127.0.0.1:27017/nerms`)})
 
 
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://158.108.34.72:4200");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
@@ -46,14 +46,14 @@ app.use('/users', users);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -63,8 +63,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, '0.0.0.0', function () {
-  console.log('Listening to port:  ' + 3000);
-});
+
 
 module.exports = app;
