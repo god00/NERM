74ffeff0e0a8c2f161da2e5ce65d66a2382e905f
God <phosawat.o@ku.th>
2018-03-13 23:19:53 +0700

    test login token

--- ENRM/src/app/home/home.component.ts
+++ ENRM/src/app/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.user = localStorage.getItem('currentUser')
-    console.log(this.user['email'])
+    console.log(this.user)
   }
 
   createModal() {
