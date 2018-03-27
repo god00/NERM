6acc1a3f982581a9521f4441d1a30c86b83578cc
God <phosawat.o@ku.th>
2018-03-28 01:55:56 +0700

    test tab in create page

--- ENRM/src/app/create-model/create-model.component.ts
+++ ENRM/src/app/create-model/create-model.component.ts
@@ -9,7 +9,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CreateModelComponent implements OnInit {
   user: Object;
   private sub: any;
-
+  corpus ;
   constructor(
     private router: Router,
     private route: ActivatedRoute
