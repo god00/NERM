f4dd1782fcdba8be7933d4409880c3fe18d07c63
God00 <phosawat.o@ku.th>
2018-04-10 22:52:06 +0700

    debug upload

--- mongodb/services/NERM.service.js
+++ mongodb/services/NERM.service.js
@@ -68,7 +68,7 @@ exports.createUser = async function (user) {
 exports.createModel = async function (nerm) {
 
     var newModel = new NERMModel({
-        email: nerm.email,
+        email: nerm.email || '',
         modelName: nerm.modelName,
         date: new Date(),
         corpus: [],
