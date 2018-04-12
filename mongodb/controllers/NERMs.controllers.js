d1ae516dc85be7a2192203f8c005c675376a2850
God00 <phosawat.o@ku.th>
2018-04-13 02:18:57 +0700

    upload test

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -182,7 +182,7 @@ exports.uploadsFile = async function (req, res, next) {
         await upload(req, res, async function (err) {
             console.log('uploading...')
             if (err) {
-                console.log('error1')
+                console.log(err.toString())
                 return res.status(205).json({ status: 205, message: err.toString() })
             }
             var query = NERMModel.findOne({ email: req.body.email[0], ModelName: req.body.modelName[0] });
