2ca5df7e369a0cc4ce167e0f203840369f9f8585
God00 <phosawat.o@ku.th>
2018-04-13 05:42:04 +0700

    get model

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -198,6 +198,7 @@ exports.uploadsFile = async function (req, res, next) {
                     if (!(`${path.dirname(process.cwd())}/storage/uploads/${req.body.email}/${req.body.modelName}/${req.files[0].originalname}` in model[mode]))
                         model[mode].push(`${path.dirname(process.cwd())}/storage/uploads/${req.body.email}/${req.body.modelName}/${req.files[0].originalname}`);
                     NERMService.updateModel(model, mode);
+                    console.log(model[mode])
                     return res.status(205).json({ status: 205, message: "File is uploaded" });
                 }
                 else {
