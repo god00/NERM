b1e949cd31042c8bbb7ec1e7e0a1d2db94220ac4
God00 <phosawat.o@ku.th>
2018-04-13 04:23:54 +0700

    get model

--- ENRM/src/app/create-model/create-model.component.ts
+++ ENRM/src/app/create-model/create-model.component.ts
@@ -43,10 +43,7 @@ export class CreateModelComponent implements OnInit {
     public databaseService: DatabaseService
   ) {
     this.user = JSON.parse(localStorage.getItem('currentUser'));
-    console.log(this.user)
-    console.log(this.model)
-    this.model['modelName'] = JSON.parse(localStorage.getItem('currentModel')).modelName;
-    console.log('after')
+    this.model.modelName = JSON.parse(localStorage.getItem('currentModel')).modelName;
   }
 
   ngOnInit() {
