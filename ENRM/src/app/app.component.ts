26f6a137b945fbbb0d29974e580fb213d215a6e9
God00 <phosawat.o@ku.th>
2018-04-14 23:15:12 +0700

    get models

--- ENRM/src/app/app.component.ts
+++ ENRM/src/app/app.component.ts
@@ -37,6 +37,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   addPathModel(modelsByUser: any) {
     for (let model of modelsByUser) {
+      console.log('addpath')
       this.router.config.unshift({ path: model.ModelName, component: CreateModelComponent })
     }
   }
