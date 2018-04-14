e0e1427138721a3f697b942208823f4b49fbc22a
God00 <phosawat.o@ku.th>
2018-04-14 20:14:00 +0700

    get models

--- ENRM/src/app/app.component.ts
+++ ENRM/src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { CreateModelComponent } from './create-model/create-model.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  user: Object;
+
+  constructor(public router: Router) {
+    this.user = JSON.parse(localStorage.getItem('currentUser'));
+  }
+
+  ngOnInit() {
+    // this.addPathModel();
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  addPathModel() {
+
+    this.router.config.unshift({ path: '', component: CreateModelComponent })
+  }
+
 }
