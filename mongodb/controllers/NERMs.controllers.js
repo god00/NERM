f178e3c4213b58a6f7b2653ea05980ec3cabd1a1
God00 <phosawat.o@ku.th>
2018-04-14 23:55:24 +0700

    models homepage

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -227,6 +227,7 @@ exports.uploadsFile = async function (req, res, next) {
 }
 
 exports.getModel = async function (req, res, next) {
+    console.log(req.param('modelName'))
     try {
         var query = NERMModel.findOne({ email: req.param('email'), ModelName: req.param('modelName') });
         query.exec(async function (err, model) {
