8a5d4d68e6f4ad9b207c7d4ae2bcfe257ed83bfb
God00 <phosawat.o@ku.th>
2018-04-14 18:58:35 +0700

    get models

--- ENRM/src/app/services/database.service.ts
+++ ENRM/src/app/services/database.service.ts
@@ -52,7 +52,7 @@ export class DatabaseService {
       }
     }).map((res) => {
       //Maps the response object sent from the server
-      return res["data"] as NERMModel[];
+      return res["data"].docs as NERMModel[];
     })
   }
 
