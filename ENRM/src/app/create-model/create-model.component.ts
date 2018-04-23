dd86bcce1a89f25420804e231c761df002cd67c4
God00 <phosawat.o@ku.th>
2018-04-24 03:03:30 +0700

    add feature

--- ENRM/src/app/create-model/create-model.component.ts
+++ ENRM/src/app/create-model/create-model.component.ts
@@ -60,7 +60,7 @@ export class CreateModelComponent implements OnInit {
       enableSearchFilter: true,
     };
     this.selectedSubscribe = this.selectedItems.valueChanges.subscribe((selected) => {
-      console.log(this.selectedItems)
+      console.log(selected)
       if (this.updateProjectSubscribe)
         this.updateProjectSubscribe.unsubscribe();
       this.project.selectedDict = this.selectedItems.value;
