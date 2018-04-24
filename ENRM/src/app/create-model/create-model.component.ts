5830e784af1c3ded82448ac9c7cc0ed3d1f86350
God00 <phosawat.o@ku.th>
2018-04-24 18:58:39 +0700

    add summit button

--- ENRM/src/app/create-model/create-model.component.ts
+++ ENRM/src/app/create-model/create-model.component.ts
@@ -123,12 +123,12 @@ export class CreateModelComponent implements OnInit, OnDestroy {
           }
 
           this.dataSource = new MatTableDataSource(this.dictFeature);
-          // if (this.project.summitPreProcessing) {
-          //   this.activeIdString = "featureSelection"
-          // }
-          // else{
-          //   this.activeIdString = "preProcess"
-          // }
+          if (this.project.summitPreProcessing) {
+            this.activeIdString = "featureSelection"
+          }
+          else{
+            this.activeIdString = "preProcess"
+          }
         }
         else {
           console.log('No model');
