f5d7d3b41f6e497edbbeb3eb102c9abecfe1472d
God00 <phosawat.o@ku.th>
2018-04-24 07:05:45 +0700

    add feature

--- ENRM/src/app/create-model/create-model.component.ts
+++ ENRM/src/app/create-model/create-model.component.ts
@@ -222,6 +222,7 @@ export class CreateModelComponent implements OnInit {
     }
     else {
       let index = this.displayedColumns.indexOf(id)
+      console.log(this.displayedColumns)
       console.log(index)
       if (index != -1)
         this.displayedColumns.splice(index, 1)
