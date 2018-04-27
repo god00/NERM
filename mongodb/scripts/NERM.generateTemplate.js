fd65dbd477074e8675a2a6e3c47296fae66622b2
God00 <phosawat.o@ku.th>
2018-04-27 22:29:03 +0700

    genarate template

--- mongodb/scripts/NERM.generateTemplate.js
+++ mongodb/scripts/NERM.generateTemplate.js
@@ -15,7 +15,7 @@ exports.genarateTemplate = async function (featureSelection, email, projectName)
           // genarate vocab template
           featureSelection.vocabFeature.forEach(item => {
             if (item.selected) {
-              await generateTemplateWithLine(item.id, 1, path).then();
+              generateTemplateWithLine(item.id, 1, path).then();
             }
           });
 
