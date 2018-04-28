7c3cf35850bd6c668440dcefbc58bfc2a841290e
God00 <phosawat.o@ku.th>
2018-04-29 05:54:18 +0700

    add generate-dict

--- ENRM/src/app/create-model/create-model.component.ts
+++ ENRM/src/app/create-model/create-model.component.ts
@@ -343,14 +343,14 @@ export class CreateModelComponent implements OnInit, OnDestroy {
       this.sortSelectedDict();
       this.updateProjectSubscribe = this.databaseService.updateNERM(this.project).subscribe((res) => {
         if (res) {
-          console.log(res.message);
-          this.initDictFeature();
-          this.dataSourceDict.data = this.dictFeature;
-          this.activeIdString = "featureSelection";
+          // console.log(res.message);
           if (this.generateDictListSubcribe)
             this.generateDictListSubcribe.unsubscribe();
           this.generateDictListSubcribe = this.databaseService.genarateDictList(this.project._id).subscribe((res) => {
-            console.log(res.message);
+            this.initDictFeature();
+            this.dataSourceDict.data = this.dictFeature;
+            this.activeIdString = "featureSelection";
+            // console.log(res.message);
           })
         }
       });
