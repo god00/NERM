788939cc3b532e0b1bc79bb29b6b6ca703b2cfea
God00 <phosawat.o@ku.th>
2018-04-30 03:23:46 +0700

    add isTraining

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -1,5 +1,3 @@
-import { AssertionError } from 'assert';
-
 // Accessing the Service that we just created
 var NERMService = require('../services/NERM.service')
 var NERMProject = require('../models/NERM.model')
