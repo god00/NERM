30aa291bf4f26a7c6916e111a5b0eacc795d00e5
God00 <phosawat.o@ku.th>
2018-04-30 17:35:53 +0700

    update model-list

---
+++ mongodb/scripts/NERM.getCorpusInfo.js
@@ -0,0 +1,26 @@
+// Get corpus info 
+const fs = require('fs');
+var path = require('path')
+
+exports.getCorpusInfo = async function (project, modelname) {
+    return new Promise((resolve, reject) => {
+        filePath = `${path.dirname(process.cwd())}/storage/uploads/${project.email}/${project.projectName}/corpus_info.txt`
+        fs.readFile(filePath, { encoding: 'utf-8' }, async function (err, data) {
+            if (!err) {
+                str = data;
+                rows = str.split('\n');
+                rows.splice(-1, 1);
+                vocabcount = rows.splice(0, 1);
+                tags = {};
+                rows.forEach(row => {
+                    arr = row.split(' ');
+                    tags[arr[0]] = arr[1];
+                });
+                resolve({ vocabcount, tags });
+            } else {
+                reject();
+            }
+        })
+    })
+
+}
