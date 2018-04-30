a8d25900d9f9415c5d31775509181cbe627c2452
God00 <phosawat.o@ku.th>
2018-04-30 23:15:23 +0700

    add upload testdata

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -197,7 +197,7 @@ exports.uploadsFile = async function (req, res, next) {
     });
     var upload = multer({ storage: storage }).any();
     upload(req, res, async function (err) {
-
+      console.log(req.files)
       checkDirectory(pathUploads + req.body.email)
         .then(() => {
           if (req.body.mode == 'dictionary') {
