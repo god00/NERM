1bc9727793381754d1a6477924753e42b2cb416a
God00 <phosawat.o@ku.th>
2018-04-30 23:31:30 +0700

    add upload testdata

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -284,7 +284,7 @@ exports.uploadsFile = async function (req, res, next) {
                             else if (project) {
                               var p = `${pathUploads}${req.body.email}/${req.body.projectName}/${req.body.mode}/${req.body.modelname}/${req.files[0].originalname}`
 
-                              if (project.testData[req.body.modelname].indexOf(p) == -1) {    //check if for no duplication path file in db
+                              if (project.testData[req.body.modelname] && project.testData[req.body.modelname].indexOf(p) == -1) {    //check if for no duplication path file in db
                                 project.testData[req.body.modelname].push(p);
                               }
                               NERMService.updateNERM(project);
