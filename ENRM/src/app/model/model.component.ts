faf3fbe57efef57a300c3af3e62446afbb0ca39b
God00 <phosawat.o@ku.th>
2018-04-30 23:44:45 +0700

    add upload testdata

--- ENRM/src/app/model/model.component.ts
+++ ENRM/src/app/model/model.component.ts
@@ -39,14 +39,14 @@ export class ModelComponent implements OnInit, OnDestroy {
     this.project.projectName = decodeURI(this.router.url.split("/")[1]);
     this.modelName = decodeURI(this.router.url.split("/")[2]);
     this.project.email = this.user['email'];
+    this.getProjectWithModelName();
   }
 
   ngOnInit() {
-    this.getProjectWithModelName();
     if (this.intervalId) {
       clearInterval(this.intervalId);
     }
-    if (this.project.isTraining && this.project.model[this.project.model.length - 1] == this.modelName) {
+    if (!this.project.isTraining && this.project.model[this.project.model.length - 1] == this.modelName) {
       this.intervalId = setInterval(() => {
         console.log('interval')
         if (this.getProjectSubscribe) {
