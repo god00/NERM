597ec28ae376fadc6b325506b6569ced4875a47a
God00 <phosawat.o@ku.th>
2018-05-02 01:36:01 +0700

    download file

--- ENRM/src/app/model/model.component.ts
+++ ENRM/src/app/model/model.component.ts
@@ -248,7 +248,7 @@ export class ModelComponent implements OnInit, OnDestroy {
       this.downloadModelSubscribe.unsubscribe();
     }
     this.downloadModelSubscribe = this.databaseService.downloadModel(this.user['email'], encodeURI(<string>this.project.projectName), this.modelName).subscribe((res) => {
-      let buffer = Buffer.from(JSON.parse(res.file));
+      let buffer = Buffer.from(res.file.data);
       console.log(buffer.toString)
       var blob = new Blob([res.file.data]);
       console.log(blob)
