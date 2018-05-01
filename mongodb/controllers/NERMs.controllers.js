c0130dfe62709b5d91dc42068b4ba532da9fc03c
God00 <phosawat.o@ku.th>
2018-05-01 21:37:58 +0700

    fix url encoder

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -746,11 +746,11 @@ async function runExtractFeaturePython_Test(testData) {
     var extractScriptPath = config.extractScriptPath;
     var pathTestData = `${path.dirname(process.cwd())}/storage/uploads/${testData.email}/${testData.projectName}/testdata/${testData.modelname}/`;
     var pathDictList = `${path.dirname(process.cwd())}/storage/uploads/${testData.email}/${testData.projectName}/${testData.modelname}_folder/current_dictlist.txt`;
-    const py = spawn('python', [extractScriptPath, pathTestData, pathDictList], { detached: true, stdio: 'ignore' });  // arg[1] : path of corpus folder , arg[2] : path of file dictionary
+    const py = spawn('python', [extractScriptPath, pathTestData, pathDictList], { detached: true });  // arg[1] : path of corpus folder , arg[2] : path of file dictionary
 
-    // py.stderr.on('data', (data) => {
-    //   console.log(`stderr: ${data}`);
-    // });
+    py.stderr.on('data', (data) => {
+      console.log(`stderr: ${data}`);
+    });
 
     py.on('exit', async (code) => {
       console.log(`child process exited with code ${code}`);
