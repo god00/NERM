7f9e55be50b53736f841e36df5b18f467813d026
God00 <phosawat.o@ku.th>
2018-05-01 22:50:57 +0700

    fix extract

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -729,7 +729,7 @@ async function runExtractFeaturePython_Test(testData, res) {
   var pathTestData = `${path.dirname(process.cwd())}/storage/uploads/${testData.email}/${testData.projectName}/testdata/${testData.modelname}/`;
   var pathDictList = `${path.dirname(process.cwd())}/storage/uploads/${testData.email}/${testData.projectName}/${testData.modelname}_folder/current_dictlist.txt`;
 
-  const py = spawn('python', [extractScriptPath, pathTestData, pathDictList], { detached: true, stdio: 'ignore' });  // arg[1] : path of corpus folder , arg[2] : path of file dictionary
+  const py = spawn('python', [extractScriptPath, pathTestData, pathDictList], { detached: true, stdio: 'ignore' });  // arg[1] : path of testdata folder , arg[2] : path of file dictionary
 
   // py.stderr.on('data', (data) => {
   //   console.log(`stderr: ${data}`, " : extract_test");
@@ -737,6 +737,7 @@ async function runExtractFeaturePython_Test(testData, res) {
 
   py.on('exit', async (code) => {
     console.log(`child process exited with code ${code}`, " : extractPython_Test");
+    await copyFeature(project.email, project.projectName, modelname);
     runTestDataPython(testData, res);
     py.kill();
   });
@@ -769,7 +770,6 @@ async function crf_learn(project, modelname) {
         project.summitPreProcessing = false;
         await checkDirectory(`${path.dirname(process.cwd())}/storage/uploads/${project.email}/${project.projectName}/${modelname}_folder`).then(async () => {
           await copyDistList(project.email, project.projectName, modelname);
-          await copyFeature(project.email, project.projectName, modelname);
           await NERMService.createModel(project.email, project.projectName, modelname);
           await NERMService.updateNERM(project);
           crf.kill()
