71235f3c1ad12d63c6a4e13bf640547c4fd97703
God00 <phosawat.o@ku.th>
2018-05-25 04:41:46 +0700

    add predict

--- ENRM/src/app/predict-page/predict-page.component.ts
+++ ENRM/src/app/predict-page/predict-page.component.ts
@@ -20,7 +20,6 @@ export class PredictPageComponent implements OnInit {
 
   onSelect(ev) {
     let target = document.getElementById('selectModel');
-    console.log(target.outerHTML)
     let projectName = target['selectedOptions'];
     let modelName = target['value'];
     console.log(projectName, "  :  ", modelName)
