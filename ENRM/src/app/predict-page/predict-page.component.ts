fe1225a68e2615a29293db3f5d252b6cdbbde8a6
God00 <phosawat.o@ku.th>
2018-05-25 08:12:17 +0700

    add predict

--- ENRM/src/app/predict-page/predict-page.component.ts
+++ ENRM/src/app/predict-page/predict-page.component.ts
@@ -124,6 +124,7 @@ export class PredictPageComponent implements OnInit, OnDestroy {
           this.predictData = data['testData'];
           this.predictDataId = data['id'];
           this.predicting = data['testing'];
+          console.log(data)
           if (data['predict']) {
             this.output = data['predict'].data.split('\n');
             console.log(this.output)
