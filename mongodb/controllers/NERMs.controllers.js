2b50fc29c82f38d32a7b885691da6b5bf296a10c
God00 <phosawat.o@ku.th>
2018-05-25 08:05:48 +0700

    add predict

--- mongodb/controllers/NERMs.controllers.js
+++ mongodb/controllers/NERMs.controllers.js
@@ -820,8 +820,10 @@ async function runExtractFeaturePython_Predict(predictData) {
     console.log(`child process exited with code ${code}`, " : extractPython_Predict");
     await moveFeature(predictData.email, predictData.projectName, predictData.modelname);
     console.log('after move')
+    console.log(fs.existsSync(`${path.dirname(process.cwd())}/storage/uploads/${predictData.email}/${predictData.projectName}/${predictData.modelname}_folder`))
     await checkDirectory(`${path.dirname(process.cwd())}/storage/uploads/${predictData.email}/${predictData.projectName}/${predictData.modelname}_folder`).then(async () => {
       console.log('after check')
+      
       runPredictData(predictData);
       py.kill();
     });
